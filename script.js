b7fbc2c7c73aeb07fa9b467ea3123a4e9f1b0224
ChietruProgramatoru <petruneamtu000@gmail.com>
2023-07-04 20:05:03 +0300

    From console logs to buttons

--- script.js
+++ script.js
@@ -11,32 +11,79 @@ function getComputerChoice() {
     }
 }
 
-function getPlayerChoice() {
-    let playerInput = prompt("Rock, paper, scissors! What do you choose?");
-    let playerInputLowercase = playerInput.toLowerCase();
-    if (playerInputLowercase == "rock" || playerInputLowercase == "scissors" || playerInputLowercase == "paper") {
-        return playerInputLowercase;
+// function getPlayerChoice() {
+//     let playerInput = prompt("Rock, paper, scissors! What do you choose?");
+//     let playerInputLowercase = playerInput.toLowerCase();
+//     if (playerInputLowercase == "rock" || playerInputLowercase == "scissors" || playerInputLowercase == "paper") {
+//         return playerInputLowercase;
+//     }
+//     else {
+//         return "Pick rock, paper or scissors.";
+//     }
+// }
+
+function playRound() {
+    let computerChoice = getComputerChoice();
+    if (playerScore >= 5) return `You win! Good job!!!`;
+    if (computerScore >= 5) return 'You lost to the PC. Kys';
+    if (playerChoice == computerChoice) return `It's a tie! Score is ${playerScore} - ${computerScore}`;
+    if (playerChoice == "rock") {
+        if (computerChoice == "paper") {
+            computerScore += 1;
+            return `You lose... Score is ${playerScore} - ${computerScore}`;
+        }
+        if (computerChoice == "scissors") {
+            playerScore += 1;
+            return `You win! Score is ${playerScore} - ${computerScore}`;
+        }
     }
-    else {
-        return "Pick rock, paper or scissors.";
+    if (playerChoice == "paper") {
+        if (computerChoice == "scissors") {
+            computerScore += 1;
+            return `You lose... Score is ${playerScore} - ${computerScore}`;
+        }
+        if (computerChoice == "rock") {
+            playerScore += 1;
+            return `You win! Score is ${playerScore} - ${computerScore}`;
+        }
+    }
+    if (playerChoice == "scissors") {
+        if (computerChoice == "paper") {
+            playerScore += 1;
+            return `You lose... Score is ${playerScore} - ${computerScore}`;
+        }
+        if (computerChoice == "rock") {
+            computerScore += 1;
+            return `You win! Score is ${playerScore} - ${computerScore}`;
+        }
     }
 }
 
-function playGame() {
-    let computerChoice = getComputerChoice();
-    let playerChoice = getPlayerChoice();
-    if (playerChoice == computerChoice) return "It's a tie!";
-    if (playerChoice == "rock") {
-        if (computerChoice == "paper") return "You lose...";
-        if (computerChoice == "scissors") return "You win!";
-    }
-    if (playerChoice == "paper") {
-        if (computerChoice == "scissors") return "You lose...";
-        if (computerChoice == "rock") return "You win!";
-    }
-    if (playerChoice == "scissors") {
-        if (computerChoice == "paper") return "You lose...";
-        if (computerChoice == "rock") return "You win!";
-    }
-}
-console.log(playGame());       
+// function game() {
+//     for (var i = 0; i < 5; i++) {
+//         console.log(playRound());
+//     }
+//     if (playerScore == computerScore) return "It's a tie! Better luck next time!";
+//     if (playerScore > computerScore) return "You won the game of Rock, paper, scissors!!!";
+//     else return "You lost to the computer, sorry";
+// }
+const score = document.querySelector('#score');
+const rock = document.querySelector('#rock');
+rock.addEventListener('click', () => {
+    playerChoice = 'rock';
+    score.innerHTML = playRound();
+});
+const paper = document.querySelector('#paper');
+paper.addEventListener('click', () => {
+    playerChoice = 'paper';
+    score.innerHTML = playRound();
+});
+const scissors = document.querySelector('#scissors');
+scissors.addEventListener('click', () => {
+    playerChoice = 'scissors';
+    score.innerHTML = playRound();
+});
+
+var playerChoice = 0;
+var playerScore = 0;
+var computerScore = 0; 
